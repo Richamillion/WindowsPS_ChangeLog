f72423961688149e766dbba47c40e7368feb2302
Richamillion <143223656+Richamillion@users.noreply.github.com>
2025-05-28 11:31:52 -0400

    Create startup_changelog.ps1

---
+++ startup_changelog.ps1
@@ -0,0 +1,4 @@
+$machine = hostname
+$dateTime = Get-Date -Format "yyyyMMdd_HHmmss"
+$changeLogsFolder = "SOMEPATH"
+Get-WmiObject -Class Win32_Product | Select-Object -Property Name, Version | Sort-Object Name | Export-Csv "$changeLogsFolder\$machine.changelog.$dateTime.csv" -NoTypeInformation
